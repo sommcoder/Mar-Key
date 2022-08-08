2e2af2557bb8a880052834d9119abadf8d7849a3
Brian Davies <brian.davies589@gmail.com>
2022-08-08 10:59:47 -0400

    Moderate code refactoring, removed excess return keywords, trying to figure out how to pass state between components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,42 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Marquee from "./components/Marquee/Marquee";
 import Keyboard from "./components/Keyboard/Keyboard";
+import { useState } from "react";
 
 function App() {
+  const [currLetter, setLetter] = useState("");
   const appTitle = "Mar-Key";
   // have a radio input to select the marquee sides the user would like to use
   const EAST_MARQUEE = true;
   const WEST_MARQUEE = true;
   const SOUTH_MARQUEE = true;
 
+  // keyboard letters:
+  const letterSet = [
+    {
+      rowNum: "row-1",
+      letters: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    },
+    {
+      rowNum: "row-2",
+      letters: ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+    },
+    {
+      rowNum: "row-3",
+      letters: ["ENTER", "z", "x", "c", "v", "b", "n", "m", "<=="],
+    },
+  ];
+
+  function addKeyToBlock(ltr) {
+    setLetter(ltr);
+  }
   return (
     <div id="app-container">
       <Header title={appTitle} />
       {EAST_MARQUEE === true ? <Marquee name="East" /> : ""}
       {WEST_MARQUEE === true ? <Marquee name="West" /> : ""}
       {SOUTH_MARQUEE === true ? <Marquee name="South" /> : ""}
-      <Keyboard />
+      <Keyboard letterSet={letterSet} addKeyToBlock />
     </div>
   );
 }
