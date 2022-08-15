8e21758a4c692276c944fa93e2ebc27987b3eb44
Brian Davies <93228227+sommcoder@users.noreply.github.com>
2022-08-15 15:02:50 -0400

    some basic changes, such as comments added for potential improvements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import "./App.css";
-import Header from "./components/Header/Header";
-import Marquee from "./components/Marquee/Marquee";
-import Keyboard from "./components/Keyboard/Keyboard";
-import { useState } from "react";
+import './App.css';
+import Header from './components/Header/Header';
+import Marquee from './components/Marquee/Marquee';
+import Keyboard from './components/Keyboard/Keyboard';
+import { useState } from 'react';
 
 /*
 IDEA:
@@ -10,46 +10,49 @@ IDEA:
 what if there were three text input lines within a FORM that corresponds with the Marquee components. 
 
 The user will ADD text to the text input lines and the app will generate a display on the users selected marquee displays based on the established SIZING for each Marquee block
-
-
 */
 
+fetch();
+
 function App() {
-  const [currLetter, setLetter] = useState("");
-  const appTitle = "Mar-Key";
+  const [currLetter, setLetter] = useState('');
+  const appTitle = 'Mar-Key';
   // have a radio input to select the marquee sides the user would like to use
-  const EAST_MARQUEE = true;
-  const WEST_MARQUEE = true;
-  const SOUTH_MARQUEE = true;
+  const EAST_MARQUEE = { isVisible: true, size: 50 };
+  const WEST_MARQUEE = { isVisible: true, size: 50 };
+  const SOUTH_MARQUEE = { isVisible: true, size: 100 };
 
   // keyboard letters:
   const letterSet = [
     {
-      rowNum: "row-1",
-      letters: ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+      rowNum: 'row-1',
+      letters: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
     },
     {
-      rowNum: "row-2",
-      letters: ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+      rowNum: 'row-2',
+      letters: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
     },
     {
-      rowNum: "row-3",
-      letters: ["ENTER", "z", "x", "c", "v", "b", "n", "m", "<=="],
+      rowNum: 'row-3',
+      letters: ['ENTER', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '<=='],
     },
   ];
 
+  /*
+  what i believe we want is to have serve BlockData.json to our App/js by REQUESTING letter sizes by looking up the letter and ensuring we are able to display the user's input based on each blocks stock.
+  */
   function addKeyToBlock(ltr) {
     setLetter(ltr);
   }
   return (
     <div id="app-container">
       <Header title={appTitle} />
-      {EAST_MARQUEE === true ? <Marquee name="East" /> : ""}
-      {WEST_MARQUEE === true ? <Marquee name="West" /> : ""}
-      {SOUTH_MARQUEE === true ? (
+      {EAST_MARQUEE.isVisible === true ? <Marquee name="East" /> : ''}
+      {WEST_MARQUEE.isVisible === true ? <Marquee name="West" /> : ''}
+      {SOUTH_MARQUEE.isVisible === true ? (
         <Marquee classname="marquee-south" name="South" />
       ) : (
-        ""
+        ''
       )}
       <Keyboard letterSet={letterSet} addKeyToBlock />
     </div>
--- src/components/Marquee/Marquee.js
+++ src/components/Marquee/Marquee.js
@@ -1,19 +1,19 @@
-import Block from "../Block/Block";
-import DisplayBtn from "../DisplayBtn/DisplayBtn";
-import ClearBtn from "../ClearBtn/ClearBtn";
-import TextBox from "../TextBox/TextBox";
-import "./Marquee.css";
+import Block from '../Block/Block';
+import DisplayBtn from '../DisplayBtn/DisplayBtn';
+import ClearBtn from '../ClearBtn/ClearBtn';
+import TextBox from '../TextBox/TextBox';
+import './Marquee.css';
 
 export default function Marquee(props) {
   const rows = 3;
 
-  const eastWestBlocks = 14;
+  const eastWestBlocks = 14; // change these to be reflective of the size of the particular letter
   const marqueeArr = [];
 
   const southBlocks = 22;
   const southMarqueeArr = [];
 
-  // the double for-loops construct the array:
+  // the double for-loops construct the array based on the number of blocks on the particular marquee
   function createMarqueeArr(arr, blks) {
     for (let r = 0; r < rows; r++) {
       arr[r] = [];
@@ -25,17 +25,17 @@ export default function Marquee(props) {
   createMarqueeArr(marqueeArr, eastWestBlocks);
   createMarqueeArr(southMarqueeArr, southBlocks);
 
-  console.log("marqueeArr:", marqueeArr);
-  console.log("southMarqueeArr:", southMarqueeArr);
+  console.log('marqueeArr:', marqueeArr);
+  console.log('southMarqueeArr:', southMarqueeArr);
 
   return (
     <div className="marquee-container" key={props.name}>
       <DisplayBtn name={props.name} />
       <div className="marquee-display-container">
-        {(props.name !== "South" ? marqueeArr : southMarqueeArr).map(
+        {(props.name !== 'South' ? marqueeArr : southMarqueeArr).map(
           (row, ind) => (
             <div key={`row-${ind}`} className={`marquee-row row-${ind}`}>
-              {row.map((block) => (
+              {row.map(block => (
                 <Block key={`row-${ind}-block-${block}`} block={block} />
               ))}
             </div>
