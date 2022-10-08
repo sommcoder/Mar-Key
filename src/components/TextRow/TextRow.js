9df39627d0bcac3a6c65727c5581186117c4c535
Brian Davies <brian.davies589@gmail.com>
2022-10-08 14:00:59 -0400

    Added readOnly to the TextRow component and useRef to be able to access the DOM node and sets its value through user input which we can then store in an array and set/update state more easily. The user now won't be able to select exactly where they'd like to change the input, only adding, deleting, submitted and the rest button should adjust the TextRow components now!

--- src/components/TextRow/TextRow.js
+++ src/components/TextRow/TextRow.js
@@ -1,73 +1,98 @@
 import data from "../../data/blockData.json";
+import { useRef } from "react";
 
 export default function TextRow(props) {
+  const textRowInputContainer = useRef(null);
+
+  // global component variables:
   const currMarqWidth = props.marqWidth;
   console.log("currMarqWidth:", currMarqWidth);
   let totalSizeBlocks = 0; // tracks curr size of the input field based on the key's size in our data
   let currInputArr = []; // tracks what's inside the input field for EACH TextRow component
   let disabled = false;
+  let selected = false;
+
+  // onFocus, switches the input selected value to true
+  function activateTextRow() {
+    selected = true;
+    console.log(selected);
+  }
+
+  // onBlur, switches the input selected value to false
+  function deactivateTextRow() {
+    selected = false;
+    console.log(selected);
+  }
 
   function validateEntry(ev) {
-    let key = ev.key.toLowerCase(); // consistency check
-    if (key === "tab") return true; // tab guard
-    console.log(ev.target.form);
+    if (!selected) return true; // input must be selected 1st as input is readOnly
+    let key = ev.key;
+    console.log("ev:", ev);
     console.log("key:", key);
-    console.log("currInputArr pre-delete:", currInputArr);
 
-    console.log(currInputArr.at(-1));
-    // handle deletions:
-    if (key === "backspace" || key === "delete") {
-      // empty array with deletion guard clause:
+    if (
+      key === "Tab" ||
+      key === "CapsLock" ||
+      key === "Shift" ||
+      key === "Unidentified" ||
+      key === "Alt" ||
+      key === "Meta"
+    )
+      return true;
+
+    if (key === "Backspace" || key === "Delete") {
       if (currInputArr.length === 0) return true;
-
-      // subtract the currInputArr's last element's size value from the totalSizeBlock tracker
-      console.log("delete: data lookup:", data[currInputArr.at(-1)]);
-      console.log(currInputArr.at(-1));
-      // 0.2 accounts for the border on boths sides:
-      totalSizeBlocks = currInputArr.length * 0.2;
-      totalSizeBlocks -= +data[currInputArr.at(-1)].size
-        .split("rem")
-        .splice(0, 1);
-      currInputArr.splice(-1); // remove from array
-      console.log("totalSizeBlocks", totalSizeBlocks);
-      return true; // exit the execution
+      let adjustedStrArr = textRowInputContainer.current.value.split("");
+      console.log(adjustedStrArr);
+      textRowInputContainer.current.value = adjustedStrArr
+        .slice(0, adjustedStrArr.length - 1)
+        .join(""); // join returns A NEW STRING
+      return true;
     }
+
+    /*
+     
+    LOOK INTO WHY THE SPACE BAR MIGHT BE CAUSING THE BROWSER TO SCROLL DOWN THE PAGE????
+     
+    */
 
     // handle too large of an input:
     if (totalSizeBlocks >= currMarqWidth) {
       console.log("input too big");
-      disabled = true; // not working!!
+      disabled = true;
       return false;
     }
 
-    // guard against special btns
-    if (
-      key === "capslock" ||
-      key === "shift" ||
-      key === "unidentified" ||
-      key === "alt" ||
-      key === "meta"
-    )
-      return false;
+    // when user adds VALID input:
+    let prevStr = ev.target.value; // what is already in the input field
+    console.log("prevStr:", prevStr);
 
-    // push to the input array:
-    console.log("data[key]:", data[key]);
-    currInputArr.push(key);
-    console.log("currInputArr end", currInputArr);
+    // we have made the input field readOnly, thus disabling the ability for the user to select where they would like to edit/input
+    // the users only options are to add, delete, clear and submit the input fields
+    textRowInputContainer.current.value += key;
 
-    // this will need to be updated when we start working with symbols and other ASCII bs:
+    console.log(
+      "textRowInputContainer.current:",
+      textRowInputContainer.current.value
+    );
+
+    let currInputStr = `${prevStr}${key}`.toLowerCase().trim();
+    currInputArr = [...currInputStr];
     totalSizeBlocks += +data[key].size.split("rem").splice(0, 1);
 
     console.log("totalSizeBlocks:", totalSizeBlocks);
-    // if the size of the inputted blocks is larger than the width of the marquee, remove
-
     console.log("post: totalSizeBlocks:", totalSizeBlocks);
 
-    // don't allow the user to enter anything into the textRow once the limit is reached!
+    return true;
   }
 
   return (
     <input
+      readOnly
+      ref={textRowInputContainer}
+      selected={selected}
+      onFocus={activateTextRow}
+      onBlur={deactivateTextRow}
       className="text-row"
       data-rowid={props.rowId}
       type="text"
@@ -76,3 +101,10 @@ export default function TextRow(props) {
     />
   );
 }
+
+// subtract the currInputArr's last element's size value from the totalSizeBlock Counter
+// b
+// totalSizeBlocks -= +data[currInputArr.at(-1)].size
+//   .split("rem")
+//   .splice(0, 1);
+// currInputArr.splice(-1); // remove from array
