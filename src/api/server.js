d55c4cffe521fb6430455b47869f578b5f531fbe
Brian Davies <brian.davies589@gmail.com>
2022-12-17 19:43:31 -0500

    Restructured the application to include a backend api and ui directories to help organize code

---
+++ src/api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const PORT = 5000; // server is listening on PORT 5000
+
+// I want the client to request a letter from the resource
+
+// I then want the server to accept the request, access the letter, read the .json file and send back the data associated with that letter to the client
+
+// every path!
+app.get("/", (req, res) => {
+  console.log("file Sent");
+  res.send("hello world!");
+});
+
+let optionsObj = {
+  root: "data",
+};
+
+// block path!
+app.get("/block", (req, res) => {
+  res.sendFile("/blockData.json", optionsObj);
+});
+
+// server is listening...
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
